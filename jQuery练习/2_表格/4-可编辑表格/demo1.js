82b7db99ae9c19df7ff8b6101ac088d3b511dadf
wuying <wuy_123@126.com>
2016-10-28 19:12:26 +0800

    jq练习题

--- jQuery练习/2_表格/4-可编辑表格/demo1.js
+++ jQuery练习/2_表格/4-可编辑表格/demo1.js
@@ -3,12 +3,14 @@ $(function(){
 	var editTd = $('td.editable');
 	editTd.on('click',function(){
 		var $tdEditd = $(this);
-		var $input = $('<input type="text">');
-		$input.css('border','none').width($(this).width()).css('background-color',$(this).css('background-color'))
-			.val($(this).text());
 		if($tdEditd.children('input[type="text"]').size() >0){
 			return false;
 		}
+
+		var $input = $('<input type="text">');
+		$input.css('border','none').width($(this).width()).css('background-color',$(this).css('background-color'))
+			.val($(this).text());
+
 		$(this).empty().append($input);
 		$input.trigger('focus');
 		$input.on('keyup', function (event) {
