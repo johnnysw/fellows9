47c3ac484752c0fa1b643eeb9ea91417c76c4a23
dongxiuling <dxl_web@163.com>
2016-12-01 16:41:50 +0800

    星巴克

--- 移动端开发/starbucks/js/index.js
+++ 移动端开发/starbucks/js/index.js
@@ -3,8 +3,23 @@
  */
 $(function(){
     eventType = isMobile() ?'tap':'click';
+    $("#tab span").on(eventType,function(){
 
-    $("#btn").on(eventType,function(){
-
+        var index = $(this).index();
+        var imgNum = $(".slide-box li").size();
+        var tabNum = $("#tab span:visible").size();
+        var num = Math.ceil(imgNum/tabNum);
+        console.log(num+"DDD"+index);
+        var iLeft = -num*290*index;
+        if(index == tabNum-1){
+            iLeft = $(".slide-box").width()-$(".slide-box ul").width();
+        }
+        $(".slide-box ul").animate({
+            left:iLeft
+        })
     })
+    $(window).resize(function(){
+        iLeft = $(".slide-box").width()-$(".slide-box ul").width();
+        $(".slide-box ul").css('left',iLeft);
+    });
 })
