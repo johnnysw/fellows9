48d5e1988bb779e66ebd888f18fac2d6b2c8df34
xiecheng <13136666328@163.com>
2016-12-08 15:09:04 +0800

    弹出层案例

---
+++ requirejs/弹出层/js/dialog.js
@@ -0,0 +1,39 @@
+define(["jquery"], function(){
+    function Dialog(settings){
+        this.defaultSettings = {
+            width: 400,
+            height: 300,
+            title: "弹出层",
+            content: ""
+        };
+        $.extend(this.defaultSettings, settings);
+        this.$container = $('<div class="dialog-container"></div>');
+        this.$mask = $('<div class="dialog-mask"></div>');
+        this.$box = $('<div class="dialog-box"></div>');
+        this.$title = $('<div class="dialog-title"></div>');
+        this.$item = $('<div class="dialog-title-item"></div>');
+        this.$close = $('<div class="dialog-title-close">[X]</div>');
+        this.$content = $('<div class="dialog-content"></div>');
+    }
+    Dialog.prototype.open = function(){
+        this.$container.append(this.$mask).append(this.$box);
+        this.$box.css({
+            width: this.defaultSettings.width,
+            height: this.defaultSettings.height
+        }).append(this.$title).append(this.$content);
+        this.$title.html(this.defaultSettings.title).append(this.$item).append(this.$close);
+        if(this.defaultSettings.content){
+            this.$content.load(this.defaultSettings.content);
+        }
+        $("body").append(this.$container);
+        var that = this;
+        this.$close.on("click", function(){
+            that.close();
+        });
+    };
+    Dialog.prototype.close = function(){
+        $(this.$container).remove();
+    };
+    return Dialog;
+
+});
---
+++ requirejs/弹出层/js/dialog1.js
@@ -0,0 +1,38 @@
+define(["jquery"], function(){
+
+    return {
+        open : function(settings){
+            var defaultSettings = {
+                width: 400,
+                height: 300,
+                title: "弹出层",
+                content: ""
+            };
+            $.extend(defaultSettings, settings);
+
+            var html = '<div class="dialog-container">'
+                 + '<div class="dialog-mask"></div>'
+                 + '<div class="dialog-box">'
+                     + '<div class="dialog-title">'
+                         + '<div class="dialog-title-item">'+ defaultSettings.title +'</div>'
+                         + '<div class="dialog-title-close">[X]</div>'
+                     + '</div>'
+                        + '<div class="dialog-content">'+ defaultSettings.content +'</div>'
+                + '</div>'
+                 + '</div>';
+            $("body").append(html);
+            $(".dialog-box").css({
+                width: defaultSettings.width,
+                height: defaultSettings.height,
+                marginLeft: -defaultSettings.width / 2,
+                marginTop: -defaultSettings.height / 2
+            });
+            $(".dialog-content").css({
+                height: defaultSettings.height - 30
+            });
+            $(".dialog-title-close").on("click", function(){
+                $(".dialog-container").remove();
+            });
+        }
+    };
+});
